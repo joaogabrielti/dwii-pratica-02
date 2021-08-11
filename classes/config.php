012219c1bbf3c11c8bdb6c58bd5c17393c5d7d39
João Gabriel Xavier <joaogabriel.informaticapgua@gmail.com>
2021-08-11 19:48:06 -0300

    commit inicial

---
+++ classes/config.php
@@ -0,0 +1,15 @@
+<?php
+
+    class config {
+
+        // Nome do DB
+        const DB_NOME = 'dwii-pratica-02';
+        // Usuário do DB
+        const DB_USUARIO = 'root';
+        // Senha do DB
+        const DB_SENHA = '';
+        // Charset da conexão PDO
+        const DB_CHARSET = 'utf8';
+		// TRUE - Apresentar erros / FALSE - Não apresentar erros
+        const DEBUG = true;
+    }
---
+++ classes/modelo/Turma.php
@@ -0,0 +1,26 @@
+<?php
+    include_once '../../global.php';
+
+    class Turma extends BD {
+        public static $tabela = 'tb_turmas';
+
+        public static function get() {
+			return parent::selectAll(self::$tabela, "ORDER BY nome");
+        }
+
+        public static function find($id) {
+            return parent::selectFind(self::$tabela, "id = $id");
+        }
+
+        public static function add($dados) {
+            return parent::insert(self::$tabela, $dados);
+        }
+
+        public static function up($id, $dados) {
+            return parent::update(self::$tabela, $dados, "id = $id");
+        }
+
+        public static function del($id) {
+            return parent::delete(self::$tabela, "id = $id");
+        }
+    }
